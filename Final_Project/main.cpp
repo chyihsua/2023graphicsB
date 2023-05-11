05bf7a5ac55e298ee4f04f97fb95f44021d32084
chyihsua <ain0965416543@gmail.com>
2023-05-11 16:11:54 +0800

    week 13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -7,10 +7,20 @@ GLMmodel * body =NULL;
 GLMmodel * Ruparm=NULL;
 GLMmodel * Luparm=NULL;
 
+int show[4]={1,0,0,0};
+
 float teapotX=0,teapotY=0;
 FILE * fout=NULL;
 FILE * fin=NULL;
 
+void keyboard(unsigned char key,int x, int y)
+{
+    if (key=='0') show[0]=!show[0];
+    if (key=='1') show[1]=!show[1];
+    if (key=='2') show[2]=!show[2];
+    if (key=='3') show[3]=!show[3];
+    glutPostRedisplay(); ///­«µe
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
@@ -24,10 +34,10 @@ void display()
     }
     glPushMatrix();
         glScalef(0.3,0.3,0.3);
-        glmDraw(Luparm,GLM_MATERIAL);
-        glmDraw(Ruparm,GLM_MATERIAL);
-        glmDraw(body,GLM_MATERIAL);
-        glmDraw(head,GLM_MATERIAL);
+        if (show[3]) glmDraw(Luparm,GLM_MATERIAL);
+        if (show[2]) glmDraw(Ruparm,GLM_MATERIAL);
+        if (show[1]) glmDraw(body,GLM_MATERIAL);
+        if (show[0]) glmDraw(head,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -44,16 +54,17 @@ void mouse(int button,int state,int x,int y)
     display();
 }
 
-void keyboard(unsigned char key,int x ,int y)
-{
-    if (fin==NULL)
-    {
-        fclose(fout);
-        fin =fopen("file4.txt","r");
-    }
-    fscanf(fin,"%f%f",&teapotX,&teapotY);
-    display();
-}
+//void keyboard(unsigned char key,int x ,int y)
+//{
+//    if (fin==NULL)
+//    {
+//        fclose(fout);
+//        fin =fopen("file4.txt","r");
+//    }
+//    fscanf(fin,"%f%f",&teapotX,&teapotY);
+//    display();
+//}
+
 
 int main(int argc,char**argv)
 {
